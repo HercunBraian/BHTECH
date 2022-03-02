aa39a23f021476ba03f6d3da0514049259b34cb8
Braian <hercunb@gmail.com>
2022-03-02 18:10:50 -0300

    Se arregla nav sticky

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-/**
+      // Sticky Navegacion Menu
+
+      let nav_offset_top = $('.header_area').height() + 50;
+
+      function navbarFixed() {
+          if ($('.header_area').length) {
+              $(window).scroll(function () {
+                  let scroll = $(window).scrollTop();
+                  if (scroll >= nav_offset_top) {
+                      $('.header_area .main-menu').addClass('navbar_fixed');
+                  } else {
+                      $('.header_area .main-menu').removeClass('navbar_fixed');
+                  }
+              })
+          }
+      }
+  
+      navbarFixed();
+      
+      /**
    * Marcas Slider
    */
  new Swiper('.clients-slider', {
@@ -29,22 +48,6 @@
     }
   });
 
-
-      // Sticky Navegacion Menu
-
-      let nav_offset_top = $('.header_area').height() + 50;
-
-      function navbarFixed() {
-          if ($('.header_area').length) {
-              $(window).scroll(function () {
-                  let scroll = $(window).scrollTop();
-                  if (scroll >= nav_offset_top) {
-                      $('.header_area .main-menu').addClass('navbar_fixed');
-                  } else {
-                      $('.header_area .main-menu').removeClass('navbar_fixed');
-                  }
-              })
-          }
-      }
   
-      navbarFixed();
+
+
